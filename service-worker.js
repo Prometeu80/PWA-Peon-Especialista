5f58499f8e2769d9e0dbeff5ce5e5265f7fd344e
Carles Nicolau <joancarlesnicolau@gmail.com>
2025-09-26 10:46:51 +0200

    Add files via upload
    Archivos iniciales de la PWA

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'peon-pwa-cache-v1';
+const urlsToCache = [
+  './',
+  './Peon_especialista_2025_ios.htm',
+  './manifest.json',
+  './icon.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
